5603f0f38964327a82231218d796f55cd0b43e28
Sara-Elna <elna.elisa@gmail.com>
2022-10-25 18:44:56 +0200

    attempt 1

--- src/sieve.py
+++ src/sieve.py
@@ -11,10 +11,18 @@ def sieve(n: int) -> list[int]:
     [2, 3, 5, 7, 11, 13]
 
     """
-    assert n > 0
+    assert n > 2
     candidates = list(range(2, n + 1))
     primes = []
 
-    # FIXME: fill out this bit
+    if len(candidates) == 0:
+        return primes
+    else:
+        if len(primes) == 0:
+            primers.append(candidates[0])
+        for c in candidates:
+            for p in primes:
+                if c // != 0:
+                    primes.append(c)
 
     return primes
